774f2ae85e5c24b252f668b715d24e581231f4ae
huiwq1990 <huiwq1990@163.com>
2014-10-03 17:25:48 +0800

    Install gradle on redhat/centos linux

---
+++ install-gradle-centos.sh
@@ -0,0 +1,15 @@
+#Install gradle on redhat/centos linux
+#ref https://gist.github.com/parzonka/9371885#file-install-gradle-centos-sh
+
+# installs to /opt/gradle
+# existing versions are not overwritten/deleted
+# seamless upgrades/downgrades
+# $GRADLE_HOME points to latest *installed* (not released)
+gradle_version=1.11
+wget -N http://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
+sudo unzip -foq gradle-${gradle_version}-all.zip -d /opt/gradle
+sudo ln -sfn gradle-${gradle_version} /opt/gradle/latest
+sudo printf "export GRADLE_HOME=/opt/gradle/latest\nexport PATH=\$PATH:\$GRADLE_HOME/bin" > /etc/profile.d/gradle.sh
+. /etc/profile.d/gradle.sh
+# check installation
+gradle -v
