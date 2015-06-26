0f8e8e3c9bafe94042e337bc2bab67c1d739a349
huiwq1990 <huiwq1990@163.com>
2015-06-27 01:17:27 +0800

    java profile

---
+++ Java/java_profile.sh
@@ -0,0 +1,9 @@
+
+PROFILE_PATH=/opt/applog/MskyLog/java_profile
+mkdir -p $PROFILE_PATH
+
+TODAY=$(date +%Y%m%d%H%M)
+
+# get all process
+jstackResult=`jps -l | awk -F ' ' '{system("jstack "$1" > '$PROFILE_PATH'/jstack."$2"'$TODAY'.log")} '`
+jmapResult=`jps -l | awk -F ' ' '{system("jmap -dump:format=b,file='$PROFILE_PATH'/jmap."$2"'$TODAY'.log "$1)} '`
